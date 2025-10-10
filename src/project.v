569b0a5bb09c2c9972d6c50c6122bbe3d258d414
Ax729 <158444078+Ax729@users.noreply.github.com>
2025-10-09 17:17:26 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_RelaxOscilator (
+module tt_RelaxOscilator (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
